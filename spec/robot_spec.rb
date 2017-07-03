fb7ed1f89d153adbd1500df7a7ecb3a0a5a9a92b
iankhor <iankhorbc.thecoder@gmail.com>
2017-07-03 23:20:31 +1000

    wrote rspec for REPORT command

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -2,5 +2,16 @@ require "./../lib/robot"
 
 describe Robot do
 
+  let(:position) { "0,1" }
+  let(:direction) { "NORTH" }
+
+  describe  "#report" do
+    context "given the REPORT command" do
+      it "prints position and direction to screen" do
+        expect(Robot.report()).to eql(:position + "," + :direction)
+      end
+    end
+  end
+
 
 end
